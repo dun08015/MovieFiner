14dbfbb6b52b613def0cb3660839b62b60396652
dun08015 <dun08015@gmail.com>
2020-05-08 17:24:54 -1000

    debug get movie detail

--- app.py
+++ app.py
@@ -50,10 +50,10 @@ def movie_detail(imdbID):
 
     print("query string: ")
 
-    print('http://www.omdbapi.com/?apikey=' + key + escape('&')+'i='+query)
+    print('http://www.omdbapi.com/?apikey=' + key + '%26i='+query)
 
     movieQueryResponse = requests.get(
-        'http://www.omdbapi.com/?apikey=' + key + escape('&')+'i='+query)
+        'http://www.omdbapi.com/?apikey=' + key + '%26i='+query)
 
     print("response: ")
 
